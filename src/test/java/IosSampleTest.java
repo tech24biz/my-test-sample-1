97e22ef58b8e7203d113456f151d89d4d3d9af8e
Vakesh <tech2bizjunkies@gmail.com>
2023-05-19 19:15:57 +0530

    Updating test cases for Chat

--- src/test/java/IosSampleTest.java
+++ src/test/java/IosSampleTest.java
@@ -218,5 +218,29 @@ public class IosSampleTest {
         }
     }
 
+    @Severity(SeverityLevel.BLOCKER)
+    @Feature("Chat")
+    @Story("Search for Chat")
+    @Description("Search for Chat and open it")
+    @Test
+    private void test4_SearchChars() {
+        try {
+            checkAppiumDriver();
+            Utils.addDelay(3000);
+            WebElement searchBox = appiumDriver.findElement(AppiumBy.accessibilityId("Search"));
+            searchBox.click();
+            Allure.step("Tapped Search to key in search characters");
+
+            Utils.addDelay(500);
+            searchBox.sendKeys("nishant");
+            Allure.step("Keyed in search characters");
+
+        } catch (Exception e) {
+            GSLogger.print(" Error-100305: " + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+
+    }
+
 }
 
